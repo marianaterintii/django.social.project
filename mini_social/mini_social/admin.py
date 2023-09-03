9a29f5753b55e7381fce8094ce661d27903e6b17
Mariana Terintii <117921869+marianaterintii@users.noreply.github.com>
2023-09-03 20:32:54 +0300

    updates

--- mini_social/mini_social/admin.py
+++ mini_social/mini_social/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import  Post, Comment
-from .models import CustomUser
+from .models import  Post, Comment, CustomUser
+
 
 admin.site.register(Post)
 admin.site.register(Comment)
